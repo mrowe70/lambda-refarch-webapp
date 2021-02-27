a31530f3eb05d6933f612e42ad8243127e416de5
Matt Rowe <matthew.rowe@ascensus.com>
2021-02-27 13:15:41 -0500

    Update frontend config

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-    "aws_user_pools_web_client_id": "4mutb9ld5e2ipov630463c4u6p",  // CognitoClientID
-    "api_base_url": "https://8xvojefnzg.execute-api.us-east-1.amazonaws.com/{StageNameParam}",  // TodoFunctionApi
+    "aws_user_pools_web_client_id": "s6ad2vk1lujieh32i7arjkda3",  // CognitoClientID
+    "api_base_url": "https://2nzqw414va.execute-api.us-east-1.amazonaws.com/{StageNameParam}",  // TodoFunctionApi
     "cognito_hosted_domain": "mytodoappdemo-lambda-refarch-webapp-1.auth.us-east-1.amazoncognito.com",  // CognitoDomainName
-    "redirect_url": "https://master.d1uwx9eq7i6sj.amplifyapp.com"  // AmplifyURL
+    "redirect_url": "https://master.d25qg1jdb3t1hs.amplifyapp.com"  // AmplifyURL
   };
   
   export default config;
