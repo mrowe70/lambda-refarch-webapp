8f99394ef4cd23e15d07acc44fa3a3a9fc297fe3
Matthew Rowe <mrowe70@gmail.com>
2021-03-08 12:21:15 -0500

    Update frontend config

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "51o6n7o9nmee568ifneefcnchj",     // CognitoClientID
-  "api_base_url": "http://127.0.0.1:3000",                                     // TodoFunctionApi
+  "api_base_url": "https://fjnctl7y2a.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-lambda-refarch-webapp-1.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d1lzo4rn01gw9f.amplifyapp.com"                                      // AmplifyURL
 };
